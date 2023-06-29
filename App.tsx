1bd6f0dcf2009aec7bceb78644375db9afa656a8
Mehmet Alemdar <alemdarmehmet6@gmail.com>
2023-06-30 01:21:16 +0300

    Create waveThingy component

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ScrollToIndex from './src/components/scrollToIndex';
+import WaveThingy from './src/components/waveThingy';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <ScrollToIndex />
+      <WaveThingy />
       <StatusBar style="auto" />
     </View>
   );
